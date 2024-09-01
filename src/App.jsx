94d4f2cf3675c5c8057e32f47cea0f6407fd1cf4
Rafshan Rakeeb <rakeebrafshan4@gmail.com>
2024-09-01 14:27:57 +0530

    check workflow

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+   
+      <h1>Rafshan Cheking</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+     
+      </div>
+      <p className="read-the-docs">
+        Learning 
+      </p>
+    </>
+  )
+}
+
+export default App
